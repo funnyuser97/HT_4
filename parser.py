cfbca70bfab13be9bb3ae91bd053fd374fca9f1c
funnyuser97 <bogdan.stupka12@gmail.com>
2017-11-14 23:57:00 +0200

    first commit

---
+++ parser.py
@@ -0,0 +1,109 @@
+import re,os,csv
+
+INPUT_FILE = "openerp-server.log"
+ALL_DATA = "./reports/all_data.csv"
+UNIQUE = "./reports/unique.csv"
+
+pattern_error = r".+ERROR.+"
+pattern_warning = r".+WARNING.+"
+pattern_critical = r".+CRITICAL.+"
+
+pattern_date = r"^.{19}"
+pattern_marker = r"[E|W|C][A-Z]{4,7}"
+pattern_description = r"[E|W|C][A-Z]{4,7}.*$"
+
+def main():
+	try:
+	    os.makedirs('reports')
+	except OSError:
+	    pass
+
+	inputfile = open(INPUT_FILE, mode='r',encoding='utf-8')
+	mytext = inputfile.read()
+
+	records_error = re.findall(pattern_error,mytext)
+	records_warning = re.findall(pattern_warning,mytext)
+	records_critical = re.findall(pattern_critical,mytext)
+
+	count_error = len(records_error)
+	count_warning = len(records_warning)
+	count_critical = len(records_critical)
+
+	print('ERROR:'+str(len(records_error)))
+	print('WARNING:'+str(len(records_warning)))
+	print('CRITICAL:'+str(len(records_critical)))
+	print('Total:'+str(len(records_critical)+len(records_warning)+len(records_error)))
+
+	inf_error = parser(count_error,0,records_error)
+	inf_warning = parser(count_warning,count_error,records_warning)
+	inf_critical = parser(count_critical,count_error+count_warning,records_critical)
+
+	file1_write(inf_error,inf_warning,inf_critical)
+
+	uni_error = uni(inf_error)
+	uni_warning = uni(inf_warning)
+	uni_critical = uni(inf_critical)
+
+	print('Unique error:'+str(len(uni_error)))
+	print('Unique warning:'+str(len(uni_warning)))
+	print('Unique critical:'+str(len(uni_critical)))
+	print('Total unique:'+str(len(uni_error)+len(uni_warning)+len(uni_critical)))
+
+	file2_write(uni_error,uni_warning,uni_critical)
+	return;
+
+def parser(count,plus,records):
+	info = []
+	records_date = []
+	records_marker = []
+	records_description = []
+
+	for i in range(count):
+		tmp1 = re.findall(pattern_date,records[i])
+		tmp2 = re.findall(pattern_marker,records[i])
+		tmp3 = re.findall(pattern_description,records[i])
+		records_date.append(tmp1[0])
+		records_marker.append(tmp2[0])
+		records_description.append(tmp3[0].replace(tmp2[0],''))
+		mydict={"id_str":i+plus,"marker":records_marker[i],"date":records_date[i],"description":records_description[i]}
+		info.append(mydict)
+	return info
+
+def uni(list_of_dict):
+	new=[]
+	count = 1
+	plus = 0
+	for t_dict in list_of_dict:
+		for n_dict in new:
+			if (n_dict.get("description")==t_dict.get("description")):
+				n_dict["count"]=n_dict.get("count")+1
+				plus=1
+		if plus!=1:
+			mydict={"count":count,"marker":t_dict.get("marker"),"date":t_dict.get("date"),"description":t_dict.get("description")}
+			new.append(mydict)
+		else: plus=0
+	return new
+
+def file1_write(inf_error,inf_warning,inf_critical):
+	with open(ALL_DATA, "w", newline="") as file:
+	    columns = ["id_str","marker", "date", "description"]
+	    writer = csv.DictWriter(file, fieldnames=columns)
+	    writer.writeheader()
+	     
+	    writer.writerows(inf_error)
+	    writer.writerows(inf_warning) 	
+	    writer.writerows(inf_critical) 	
+	return;
+
+
+def file2_write(inf_error,inf_warning,inf_critical):
+	with open(UNIQUE, "w", newline="") as file:
+	    columns = ["count","marker", "date", "description"]
+	    writer = csv.DictWriter(file, fieldnames=columns)
+	    writer.writeheader()
+	     
+	    writer.writerows(inf_error)
+	    writer.writerows(inf_warning) 	
+	    writer.writerows(inf_critical)
+
+main()
